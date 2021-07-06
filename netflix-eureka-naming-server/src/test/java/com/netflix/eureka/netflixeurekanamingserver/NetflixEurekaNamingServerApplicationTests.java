47deb4f9cbd19092ab98fc60d07ebbd654956f15
jaiswalshashank <shashankjai2012@gmail.com>
2021-07-07 01:14:50 +0530

    Add files via upload

---
+++ netflix-eureka-naming-server/src/test/java/com/netflix/eureka/netflixeurekanamingserver/NetflixEurekaNamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.netflix.eureka.netflixeurekanamingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetflixEurekaNamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
